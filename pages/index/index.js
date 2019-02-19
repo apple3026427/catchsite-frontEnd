7c69611fa89f2f7e2682ca996de54335add4749d
wr <15812829247@163.com>
2019-02-19 17:13:28 +0800

    查询信息完善

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,45 +8,15 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     jobInfo: [],
-    fortest: "testtest",
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    // wx.navigateTo({
+    //   url: '../logs/logs'
+    // })
   },
   onLoad () {
-    console.log('testokkk')
     this.getJobInfo();
-    console.log(this.jobInfo);
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-
   },
   getUserInfo: function(e) {
     console.log(e)
@@ -58,14 +28,12 @@ Page({
   },
   getJobInfo() {
     let that = this;
-    
     wx.request({
       url: 'http://localhost:8080/testse',
       success: function (res) {
         console.log(res)// 服务器回包信息
         that.setData({
           jobInfo: res.data,
-          fortest: "进入方法里"
         });
       }
     })
